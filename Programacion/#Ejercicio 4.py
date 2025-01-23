2a22c4dfd19cba72136479321ab74917f47259eb
RosarioAHS <rosariiopochiherrera@gmail.com>
2025-01-23 17:35:56 +0100

    Delete Programacion/#Ejercicio 4.py

--- Programacion/#Ejercicio 4.py
+++
@@ -1,9 +0,0 @@
-#Ejercicio 4
-
-def invertir_texto(texto):
-
-    return texto[::-1] #el :: indica que selecciono toda la fila, el -1 para que recorra la fila de izq a der osea al reves.
-
-texto = "Jamon"
-texto_invertido = invertir_texto(texto)
-print(texto_invertido)
