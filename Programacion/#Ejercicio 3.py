bb5760c0262fe62cecdb1382b0c79b7ab88dfe4f
RosarioAHS <rosariiopochiherrera@gmail.com>
2025-01-23 17:35:47 +0100

    Delete Programacion/#Ejercicio 3.py

--- Programacion/#Ejercicio 3.py
+++
@@ -1,7 +0,0 @@
-#Ejercicio 3 
-def contar_a(cadena):
-    return cadena.count("a")  # cuenta "a"
-
-texto = "Jaaaamon"
-resultado = contar_a(texto)
-print(f"La cadena contiene {resultado} letras 'a'.")
